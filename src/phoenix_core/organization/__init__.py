3a0bd4e86c3c751115ce460f0a1af48d9a4db9d9
Jaco Lombard <jaco.lombard@me.com>
2026-09-05 14:16:42 +0200

    Add Core organization access scope package

---
+++ src/phoenix_core/organization/__init__.py
@@ -0,0 +1,17 @@
+"""Core organization and access-scope domain."""
+
+from .domain import (
+    AccessScope,
+    Assignment,
+    AssignmentType,
+    OrganizationUnit,
+    OrganizationUnitType,
+)
+
+__all__ = [
+    "AccessScope",
+    "Assignment",
+    "AssignmentType",
+    "OrganizationUnit",
+    "OrganizationUnitType",
+]
