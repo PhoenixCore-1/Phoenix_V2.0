c502e65eadedcc3756151eb675f785f4edbe8b48
Jaco Lombard <jaco.lombard@me.com>
2026-09-05 20:30:28 +0200

    Export Core module invocation runtime

--- src/phoenix_framework/integration/__init__.py
+++ src/phoenix_framework/integration/__init__.py
@@ -4,8 +4,10 @@ from phoenix_framework.integration.delivery import (
     EventDeliverySchedule,
     EventDeliveryScheduler,
 )
+from phoenix_framework.integration.runtime import ModuleInvocationRuntime
 
 __all__ = [
     "EventDeliverySchedule",
     "EventDeliveryScheduler",
+    "ModuleInvocationRuntime",
 ]
