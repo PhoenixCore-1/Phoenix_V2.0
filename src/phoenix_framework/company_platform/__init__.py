32f027bd0b3b2bd8eed2145dd5a8e566a402caf0
Jaco Lombard <jaco.lombard@me.com>
2026-09-08 05:58:41 +0200

    feat(company-platform): export CP-015 user operations

--- src/phoenix_framework/company_platform/__init__.py
+++ src/phoenix_framework/company_platform/__init__.py
@@ -4,7 +4,7 @@ from phoenix_framework.company_platform.access import CompanyAccessService, Comp
 from phoenix_framework.company_platform.activity import CompanyActivityAdministrationService, CompanyActivityItem, CompanyActivityView
 from phoenix_framework.company_platform.administration import CompanyAdministrationService, CompanyAdministrationView
 from phoenix_framework.company_platform.audit import CompanyAuditItem, CompanyAuditService, CompanyAuditView
-from phoenix_framework.company_platform.actions import CompanyAdministrationAction, CompanyAdministrationExecutor, CompanyAdministrationRequest, CompanyAdministrationResult
+from phoenix_framework.company_platform.actions import CompanyAdministrationAction, CompanyAdministrationExecutor, CompanyAdministrationRequest, CompanyAdministrationResult, CompanyUserAction, CompanyUserAdministrationExecutor, CompanyUserAdministrationRequest, CompanyUserAdministrationService
 from phoenix_framework.company_platform.contracts import COMPANY_PLATFORM_NAVIGATION, CompanyPlatformCapabilities, CompanyPlatformContract
 from phoenix_framework.company_platform.context import CompanyPlatformContext
 from phoenix_framework.company_platform.dashboard import CompanyDashboardService, CompanyDashboardSnapshot, CompanyDashboardView
@@ -12,7 +12,7 @@ from phoenix_framework.company_platform.navigation import CompanyNavigationServi
 from phoenix_framework.company_platform.notifications import CompanyNotificationItem, CompanyNotificationsService, CompanyNotificationsView
 from phoenix_framework.company_platform.reporting import CompanyReportDefinitionView, CompanyReportResultView, CompanyReportingService, CompanyReportingView
 from phoenix_framework.company_platform.roles import CompanyPermissionView, CompanyRoleView, CompanyRolesPermissionsService, CompanyRolesPermissionsView
-from phoenix_framework.company_platform.users import CompanyUserAdministrationService, CompanyUserView, CompanyUsersView
+from phoenix_framework.company_platform.users import CompanyUserAdministrationService as CompanyUserReadService, CompanyUserView, CompanyUsersView
 from phoenix_framework.company_platform.visibility import DataVisibilityAdministrationService, DataVisibilityRuleView, DataVisibilityView
 from phoenix_framework.company_platform.workspaces import CompanyWorkspaceAdministrationService, CompanyWorkspacesView, WorkspaceView
 
@@ -22,6 +22,7 @@ __all__ = [
     "CompanyAuditItem", "CompanyAuditService", "CompanyAuditView", "CompanyCapabilityAccess", "CompanyDashboardService", "CompanyDashboardSnapshot", "CompanyDashboardView",
     "CompanyNavigationService", "CompanyNavigationView", "CompanyNotificationItem", "CompanyNotificationsService", "CompanyNotificationsView", "CompanyPermissionView",
     "CompanyPlatformCapabilities", "CompanyPlatformContract", "CompanyPlatformContext", "CompanyReportDefinitionView", "CompanyReportResultView", "CompanyReportingService",
-    "CompanyReportingView", "CompanyRoleView", "CompanyRolesPermissionsService", "CompanyRolesPermissionsView", "CompanyUserAdministrationService", "CompanyUserView", "CompanyUsersView",
-    "DataVisibilityAdministrationService", "DataVisibilityRuleView", "DataVisibilityView", "CompanyWorkspaceAdministrationService", "CompanyWorkspacesView", "WorkspaceView",
+    "CompanyReportingView", "CompanyRoleView", "CompanyRolesPermissionsService", "CompanyRolesPermissionsView", "CompanyUserAction", "CompanyUserAdministrationExecutor",
+    "CompanyUserAdministrationRequest", "CompanyUserAdministrationService", "CompanyUserReadService", "CompanyUserView", "CompanyUsersView", "DataVisibilityAdministrationService",
+    "DataVisibilityRuleView", "DataVisibilityView", "CompanyWorkspaceAdministrationService", "CompanyWorkspacesView", "WorkspaceView",
 ]
