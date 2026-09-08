1304f25eb27d791e02d89d31a244b8be732f830b
Jaco Lombard <jaco.lombard@me.com>
2026-09-08 06:03:35 +0200

    CP-018 export workspace administration boundary

--- src/phoenix_framework/company_platform/__init__.py
+++ src/phoenix_framework/company_platform/__init__.py
@@ -17,6 +17,7 @@ from phoenix_framework.company_platform.users import CompanyUserAdministrationSe
 from phoenix_framework.company_platform.visibility import DataVisibilityAdministrationService, DataVisibilityRuleView, DataVisibilityView
 from phoenix_framework.company_platform.visibility_admin import CompanyVisibilityAction, CompanyVisibilityAdministrationExecutor, CompanyVisibilityAdministrationRequest, CompanyVisibilityAdministrationResult, CompanyVisibilityAdministrationService
 from phoenix_framework.company_platform.workspaces import CompanyWorkspaceAdministrationService, CompanyWorkspacesView, WorkspaceView
+from phoenix_framework.company_platform.workspaces_admin import CompanyWorkspaceAction, CompanyWorkspaceAdministrationExecutor, CompanyWorkspaceAdministrationOperationService, CompanyWorkspaceAdministrationRequest, CompanyWorkspaceAdministrationResult
 
 __all__ = [
     "COMPANY_PLATFORM_NAVIGATION", "CompanyAccessService", "CompanyActivityAdministrationService", "CompanyActivityItem", "CompanyActivityView",
@@ -27,6 +28,7 @@ __all__ = [
     "CompanyReportingView", "CompanyRoleAction", "CompanyRoleAdministrationExecutor", "CompanyRoleAdministrationRequest", "CompanyRoleAdministrationService", "CompanyRoleView",
     "CompanyRolesPermissionsService", "CompanyRolesPermissionsView", "CompanyUserAction", "CompanyUserAdministrationExecutor", "CompanyUserAdministrationOperationService", "CompanyUserAdministrationRequest",
     "CompanyUserAdministrationService", "CompanyUserView", "CompanyUsersView", "CompanyVisibilityAction", "CompanyVisibilityAdministrationExecutor", "CompanyVisibilityAdministrationRequest",
-    "CompanyVisibilityAdministrationResult", "CompanyVisibilityAdministrationService", "DataVisibilityAdministrationService", "DataVisibilityRuleView", "DataVisibilityView",
-    "CompanyWorkspaceAdministrationService", "CompanyWorkspacesView", "WorkspaceView",
+    "CompanyVisibilityAdministrationResult", "CompanyVisibilityAdministrationService", "CompanyWorkspaceAction", "CompanyWorkspaceAdministrationExecutor", "CompanyWorkspaceAdministrationOperationService",
+    "CompanyWorkspaceAdministrationRequest", "CompanyWorkspaceAdministrationResult", "CompanyWorkspaceAdministrationService", "CompanyWorkspacesView", "DataVisibilityAdministrationService", "DataVisibilityRuleView", "DataVisibilityView",
+    "WorkspaceView",
 ]
