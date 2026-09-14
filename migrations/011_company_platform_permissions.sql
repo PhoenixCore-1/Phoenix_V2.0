71debf27b4fc73444d0521a06e8e08c10a08d699
Jaco Lombard <jaco.lombard@me.com>
2026-09-14 12:24:14 +0200

    Fix Company Platform permission identifiers

--- migrations/011_company_platform_permissions.sql
+++ migrations/011_company_platform_permissions.sql
@@ -2,11 +2,11 @@
 -- These permissions govern tenant-level administration only. They do not grant
 -- Phoenix platform-control authority such as licensing or module activation.
 INSERT OR IGNORE INTO permissions(id, code, name, created_at) VALUES
-    ('company-perm-activity-view', 'company.activity.view', 'View company activity', CURRENT_TIMESTAMP),
-    ('company-perm-configuration-manage', 'company.configuration.manage', 'Manage company configuration', CURRENT_TIMESTAMP),
-    ('company-perm-memberships-manage', 'company.memberships.manage', 'Manage company memberships', CURRENT_TIMESTAMP),
-    ('company-perm-reports-view', 'company.reports.view', 'View company reports', CURRENT_TIMESTAMP),
-    ('company-perm-roles-manage', 'company.roles.manage', 'Manage company roles', CURRENT_TIMESTAMP),
-    ('company-perm-users-manage', 'company.users.manage', 'Manage company users', CURRENT_TIMESTAMP),
-    ('company-perm-visibility-manage', 'company.visibility.manage', 'Manage company data visibility', CURRENT_TIMESTAMP),
-    ('company-perm-workspaces-manage', 'company.workspaces.manage', 'Manage company workspaces', CURRENT_TIMESTAMP);
+    ('11111111-1111-4111-8111-000000000001', 'company.activity.view', 'View company activity', CURRENT_TIMESTAMP),
+    ('11111111-1111-4111-8111-000000000002', 'company.configuration.manage', 'Manage company configuration', CURRENT_TIMESTAMP),
+    ('11111111-1111-4111-8111-000000000003', 'company.memberships.manage', 'Manage company memberships', CURRENT_TIMESTAMP),
+    ('11111111-1111-4111-8111-000000000004', 'company.reports.view', 'View company reports', CURRENT_TIMESTAMP),
+    ('11111111-1111-4111-8111-000000000005', 'company.roles.manage', 'Manage company roles', CURRENT_TIMESTAMP),
+    ('11111111-1111-4111-8111-000000000006', 'company.users.manage', 'Manage company users', CURRENT_TIMESTAMP),
+    ('11111111-1111-4111-8111-000000000007', 'company.visibility.manage', 'Manage company data visibility', CURRENT_TIMESTAMP),
+    ('11111111-1111-4111-8111-000000000008', 'company.workspaces.manage', 'Manage company workspaces', CURRENT_TIMESTAMP);
